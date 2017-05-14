8b87581ee8d0b0450bc3a547d94247ced9468bd8
tkzw <k.takazawa811@gmail.com>
2017-05-14 11:14:17 +0900

    new

---
+++ DrawHtml.js
@@ -0,0 +1,48 @@
+/**
+ * DOM操作によるHTML描画を行うクラス
+ * 
+ */
+class DrawHtml  {
+    /**
+     * @constructor
+     */
+    constructor () {
+         
+    }
+
+    /**
+     * 初期設定描画処理
+     */
+    drawInitialSetting(){
+
+    }
+
+    /**
+     * 寄付きシグナル描画処理
+     */
+    drawSignal(){
+
+    }
+
+    /**
+     * 時刻価格描画処理
+     */
+    drawTime(){
+
+    }
+
+    /**
+     * ポジション描画処理
+     */
+    drawPosition(){
+
+    } 
+
+    /**
+     * トレード履歴描画処理
+     */
+    drawHistory(){
+
+    }      
+
+}
---
+++ TimeKeeper.js
@@ -0,0 +1,20 @@
+/**
+ * Ayumiの中心を司るクラス。主な機能は時計制御。
+ * 
+ */
+class TimeKeeper  {
+    /**
+     * @constructor
+     */
+    constructor () {
+         
+    }
+
+    /**
+     * 時間制御
+     */
+    setInterval(){
+
+    }
+
+}
