245bd50eb24fe09acf187330119ca1f8b9e1c108
tkzw <k.takazawa811@gmail.com>
2017-05-15 14:18:24 +0900

    update

--- UserAction.js
+++ UserAction.js
@@ -19,23 +19,68 @@ class UserAction  {
 
     /**
      * 開始前にデータを準備する
+     * チャートデータ取得、日足データ描画など
      */
-    xxxxxxxxxxxx(){
+    prepareData(){
 
     }
 
     /**
      * トレードを開始する
      */
-    startTrade(){
+    beginTrade(){
 
     }
 
     /**
      * トレードを終了する
      */
-    drawIndicator(){
+    endTrade(){
 
     }  
 
+    /**
+     * 一本戻る
+     */
+    prevCandlestick(){
+
+    }  
+
+    /**
+     * 一本進む
+     */
+    nextCandlestick(){
+
+    }  
+
+    /**
+     * 一時停止する
+     */
+    pauseTrade(){
+
+    }  
+
+    /**
+     * 成行で売り注文を出す
+     * 新規/決済で共通
+     */
+    sellOrder(){
+
+    }  
+
+    /**
+     * 成行で買い注文を出す
+     * 新規/決済で共通
+    */
+    buyOrder(){
+
+    }  
+
+    /**
+     * IFDOCO注文を出す
+     */
+    ifdocoOrder(){
+
+    }  
+    
 }
